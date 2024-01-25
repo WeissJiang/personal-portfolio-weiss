66b08daa5b76dcdb434cc0b0e6951c0bac70fd08
Weiss Jiang <meweissjiang@gmail.com>
2024-01-25 12:34:24 +1100

    created a cloud rain component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
+import './Home.scss';
+
 export default function Home(){
     return (
-        <>
-        <h1>Home Page</h1>
-        </>
+        <div className="home-page">
+            <div className="cloud-rain-container">
+                <div className="cloud">
+                    <p className='text'>Cloud Rain</p>
+                    {/* https://www.youtube.com/watch?v=Bl37L37Up5w */}
+                </div>
+            </div>
+        </div>
     )
 }
